959407090fa2bd798c7040861602ba7305e4d54f
shivam-24-deep <shivammogha73@gmail.com>
2025-04-06 21:44:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,30 @@
-const CACHE_NAME = "swasthai-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-  // Add more assets like CSS, JS, images if needed
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('swasthai-cache-v1').then(function(cache) {
+        return cache.addAll([
+          'index.html',
+          'manifest.json',
+          'icon-192.png',
+          'icon-512.png',
+          'bmi.html',
+          'calendar.html',
+          'clinics.html',
+          'feeling-lucky.html',
+          'health-checklist.html',
+          'HealthTips.html',
+          'language.html',
+          'referral.html',
+          'voice.html'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
